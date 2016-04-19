d13d6ecc28d89754e3e644269dccc99a8c7533c7
arxangel008 <valera-bapbap56@mail.ru>
2016-04-19 19:59:27 +0700

    step three

---
+++ bower-grails-asset-pipeline/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,24 @@
+grails.project.work.dir = 'target'
+
+grails.project.dependency.resolution = {
+
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsCentral()
+		mavenCentral()
+	}
+
+	dependencies {
+		//compile 'com.bertramlabs.plugins:coffee-asset-pipeline:2.7.2'
+	}
+	plugins {
+
+		runtime ":asset-pipeline:2.7.2"
+
+		build ':release:3.1.2', ':rest-client-builder:2.0.1', {
+            export = false
+        }
+	}
+}
